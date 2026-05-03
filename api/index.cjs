cceac92e7091b329a0b8105f08079eeab033b706
G0D-D3M0N <bharadshyam456@gmail.com>
2026-05-03 23:32:25 +0530

    Use supported mjs Vercel function wrapper for cjs server bundle

--- api/index.cjs
+++
@@ -1,5 +0,0 @@
-// Vercel Node function entrypoint.
-// The build step generates dist/server.cjs.
-const serverModule = require("../dist/server.cjs");
-
-module.exports = serverModule.default || serverModule;
